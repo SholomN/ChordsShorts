c41484be3651221988c81da9ced5ad7b09c527a8
SholomN <sholomhass@gmail.com>
2024-11-29 00:39:46 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function insertChord() {
+    const chord = document.getElementById("chord").value;
+
+    // This is a placeholder for the actual Word interaction
+    // Later, we'll need to implement the logic to insert the chord above the selected text
+    console.log(`Chord selected: ${chord}`);
+}
